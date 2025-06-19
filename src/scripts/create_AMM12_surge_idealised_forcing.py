e04391f77241330a0efd18f4203e7dd6e357c5a0
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-06-19 22:49:45 +0100

    Update idealised forcing script to handle 0-360 longitudes and add synthetic storm on days 5-10.

--- src/scripts/create_AMM12_surge_idealised_forcing.py
+++ src/scripts/create_AMM12_surge_idealised_forcing.py
@@ -6,12 +6,12 @@
 #
 # -------------------------------------------------
 # -- Import required libraries -- #
+import os 
 import glob
 import pandas as pd
 import xarray as xr
 import numpy as np
 from paratc.tc_models import Holland1980 as h80
-import os 
 
 def main(run_dir: str) -> None:
     """
@@ -34,14 +34,13 @@ def main(run_dir: str) -> None:
 
     # -- Import Existing SETTE AMM12 Atmospheric Forcing File -- #
     forcing_fpath = f"{run_dir}/fluxes/amm12_utau_y2012m01d*.nc"
-    # Open existing atm. forcing file and select first 120 hours (5-days):
+    # Open existing atm. forcing files:
     ds_atm = xr.open_mfdataset(forcing_fpath)
-    ds_atm = ds_atm.isel(t=slice(0, 120))
 
     # -- Create synthetic storm track -- #
     # Synthetic storm moving zonally across model domain:
     dx_dt = 33/120
-    lon_start = ds_domain.nav_lon.isel(y=50, x=0).values
+    lon_start = (((ds_domain.nav_lon.isel(y=50, x=0) + 180) % 360) - 180).values
     lon = lon_start + (np.arange(120) * dx_dt)
     lon = lon % 360
     # Update final longitude for storm to exit domain:
@@ -51,8 +50,8 @@ def main(run_dir: str) -> None:
     lat = np.repeat(50, len(lon))
     pcen = np.repeat(985, len(lon)) # Central pressure in hPa
     penv = np.repeat(1010, len(lon)) # Background pressure in hPa
-    rmw = np.repeat(50, len(lon)) # Radius of maximum wind in km
-    time = ds_atm['t'].values 
+    rmw = np.repeat(60, len(lon)) # Radius of maximum wind in km
+    time = ds_atm['t'].values[120:] # Last 5-days
 
     # Create a DataFrame with storm parameters:
     df = pd.DataFrame({
@@ -66,7 +65,11 @@ def main(run_dir: str) -> None:
 
     # -- Generate synthetic storm wind + pressure fields using Holland (1980) model -- #
     # Define idealised storm using Holland (1980) model:
-    storm = h80(df, ds_domain.nav_lon.values, ds_domain.nav_lat.values, B_model='vickery00')
+    storm = h80(df,
+                (((ds_domain.nav_lon + 180) % 360) - 180).values,
+                ds_domain.nav_lat.values,
+                B_model='vickery00'
+                )
 
     # Calculate wind stress using Large Pond (1982) drag coefficient model:
     storm.make_wind_stress(cd_model = 'large_pond82', cd_max = 3e-3)
@@ -75,16 +78,21 @@ def main(run_dir: str) -> None:
 
     # === Zonal Wind Stress === #
     print("In Progress: Exporting synthetic storm zonal wind stress (N/m2) to NETCDF files...")
-    # Collect first 5-days of SETTE AMM12 atm. forcing filenames:
-    fnames = sorted(glob.glob(f"{run_dir}/fluxes/amm12_utau_y2012m01d*.nc"))[:5]
+    # Collect first 10-days of SETTE AMM12 atm. forcing filenames:
+    fnames = sorted(glob.glob(f"{run_dir}/fluxes/amm12_utau_y2012m01d*.nc"))
 
     # Define output filenames:
     out_fnames = [f.replace('amm12_', 'amm12_surge_') for f in fnames]
 
-    # Iterate over 5-days of existing files and create idealised atm. forcing files:
+    # Iterate over 10-days of existing files and create idealised atm. forcing files:
     for n, f in enumerate(fnames):
         # Construct dataset from synthetic storm data:
-        ds_n = xr.Dataset(data_vars={"utau": (['t', 'y', 'x'], storm.data.isel(time=slice(n*24, n*24 + 24)).stress_u.data)},
+        if n < 5:
+            utau = storm.data.isel(time=slice(0, 24)).stress_u.data * 0.0  # Set to 0 N m-2 for first 5 days.
+        else:
+            utau = storm.data.isel(time=slice((n - 5)*24, (n - 5)*24 + 24)).stress_u.data
+
+        ds_n = xr.Dataset(data_vars={"utau": (['t', 'y', 'x'], utau)},
                         coords={"nav_lat": (['y', 'x'], ds_domain.nav_lat.squeeze().data),
                                 "nav_lon": (['y', 'x'], ds_domain.nav_lon.squeeze().data),
                                 "t": ds_atm["t"].isel(t=slice(n*24, n*24 + 24)).data
@@ -96,16 +104,21 @@ def main(run_dir: str) -> None:
 
     # === Meridional Wind Stress === #
     print("In Progress: Exporting synthetic storm meridional wind stress (N/m2) to NETCDF files...")
-    # Collect first 5-days of SETTE AMM12 atm. forcing filenames:
-    fnames = sorted(glob.glob(f"{run_dir}/fluxes/amm12_vtau_y2012m01d*.nc"))[:5]
+    # Collect 10-days of SETTE AMM12 atm. forcing filenames:
+    fnames = sorted(glob.glob(f"{run_dir}/fluxes/amm12_vtau_y2012m01d*.nc"))
 
     # Define output filenames:
     out_fnames = [f.replace('amm12_', 'amm12_surge_') for f in fnames]
 
-    # Iterate over 5-days of existing files and create idealised atm. forcing files:
+    # Iterate over 10-days of existing files and create idealised atm. forcing files:
     for n, f in enumerate(fnames):
         # Construct dataset from synthetic storm data:
-        ds_n = xr.Dataset(data_vars={"vtau": (['t', 'y', 'x'], storm.data.isel(time=slice(n*24, n*24 + 24)).stress_v.data)},
+        if n < 5:
+            vtau = storm.data.isel(time=slice(0, 24)).stress_v.data * 0.0  # Set to 0 N m-2 for first 5 days.
+        else:
+            vtau = storm.data.isel(time=slice((n - 5)*24, (n - 5)*24 + 24)).stress_v.data
+
+        ds_n = xr.Dataset(data_vars={"vtau": (['t', 'y', 'x'], vtau)},
                         coords={"nav_lat": (['y', 'x'], ds_domain.nav_lat.squeeze().data),
                                 "nav_lon": (['y', 'x'], ds_domain.nav_lon.squeeze().data),
                                 "t": ds_atm["t"].isel(t=slice(n*24, n*24 + 24)).data
@@ -119,14 +132,17 @@ def main(run_dir: str) -> None:
     # Define output filenames:
     out_fnames = [f.replace('amm12_', 'amm12_surge_').replace('vtau', 'somslpre') for f in fnames]
 
-    # Iterate over 5-days of existing files and create idealised atm. forcing files:
+    # Iterate over 10-days of existing files and create idealised atm. forcing files:
     print("In Progress: Exporting synthetic storm atmospheric pressure (Pa) to NETCDF files...")
     for n, f in enumerate(fnames):
-        # Calulate sea-level pressure from storm data:
-        somslpre = storm.data.isel(time=slice(n*24, n*24 + 24)).pressure * 100  # Convert hPa to Pa
-
         # Construct dataset from synthetic storm data:
-        ds_n = xr.Dataset(data_vars={"somslpre": (['t', 'y', 'x'], somslpre.data)},
+        if n < 5:
+            somslpre = storm.data.isel(time=slice(0, 24)).pressure.data * 0.0  # Set to 0 Pa for first 5 days.
+        else:
+            # Calulate sea-level pressure from storm data:
+            somslpre = storm.data.isel(time=slice((n - 5)*24, (n - 5)*24 + 24)).pressure.data * 100  # Convert hPa to Pa
+
+        ds_n = xr.Dataset(data_vars={"somslpre": (['t', 'y', 'x'], somslpre)},
                         coords={"nav_lat": (['y', 'x'], ds_domain.nav_lat.squeeze().data),
                                 "nav_lon": (['y', 'x'], ds_domain.nav_lon.squeeze().data),
                                 "t": ds_atm["t"].isel(t=slice(n*24, n*24 + 24)).data
@@ -138,8 +154,8 @@ def main(run_dir: str) -> None:
 
 # Entry point for the script:
 if __name__ == "__main__":
-    # Define NEMO AMM12 SURGE run directory path:
-    run_dir = "/".join(os.getcwd().split('/')[:-2]) + "/nemo_5.0.1/cfgs/AMM12_SURGE/EXP00"
+    # Define NEMO AMM12_SURGE run directory path:
+    run_dir = f"{'/'.join(os.getcwd().split('/')[:-2])}/nemo_5.0.1/cfgs/AMM12_SURGE/EXP00"
 
     # Create idealised atmospheric forcing files:
     main(run_dir=run_dir)
